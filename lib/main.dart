ab2b3f1e4b3724984e054340fe609a5fb3c90516
nayemsabur <98073930+nayemsabur@users.noreply.github.com>
2023-11-03 23:16:07 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Weather Information'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Map<String, dynamic>> cities = [
+    {
+      "city": "New York",
+      "temperature": 20,
+      "condition": "Clear",
+      "humidity": 60,
+      "windSpeed": 5.5
+    },
+    {
+      "city": "Los Angeles",
+      "temperature": 25,
+      "condition": "Sunny",
+      "humidity": 50,
+      "windSpeed": 6.8
+    },
+    {
+      "city": "London",
+      "temperature": 15,
+      "condition": "Partly Cloudy",
+      "humidity": 70,
+      "windSpeed": 4.2
+    },
+    {
+      "city": "Tokyo",
+      "temperature": 28,
+      "condition": "Rainy",
+      "humidity": 75,
+      "windSpeed": 8.0
+    },
+    {
+      "city": "Sydney",
+      "temperature": 22,
+      "condition": "Cloudy",
+      "humidity": 55,
+      "windSpeed": 7.3
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: cities.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(
+                cities[index]['city'],
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Temperature: ${cities[index]['temperature']} °C'),
+                  Text('Condition: ${cities[index]['condition']}'),
+                  Text('Humidity: ${cities[index]['humidity']} %'),
+                  Text('Wind Speed: ${cities[index]['windSpeed']} km/h'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
